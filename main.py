98c20f79f14b60fce547cd9864ca120c27aa784e
Madhulika Damor <51967183+madhudamor24@users.noreply.github.com>
2022-01-18 12:31:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from flask import Flask, redirect,  render_template, request
-
 app = Flask(__name__)
 
 # encryption-decription process
 def encryption_decryption(text):
     res = []
     for i in range(len(text)):
-        ch = chr(ord('z') - ord(text[i]) + ord('a'))
-        res.append(ch)
+        if text[i]>='a' and text[i]<='z':
+            ch = chr(ord('z') - ord(text[i]) + ord('a'))
+            res.append(ch)
+        elif text[i]>='A' and text[i]<='Z':
+            ch = chr(ord('Z') - ord(text[i]) + ord('A'))
+            res.append(ch)
+        elif text[i]==" ":
+            res.append(" ")
     return ''.join(res)
-
 
 # convert plaintext to ciphertext
 @app.route("/", methods = ["POST", "GET"])
@@ -20,14 +24,12 @@ def encipher():
     else:
         return render_template("index.html")
 
-
-# convert ciphertext to plintext
+# convert ciphertext to plaintext
 @app.route("/decipher", methods = ["POST"])
 def decipher():
     cipher_text = request.form['ciphertext']
     print("d", cipher_text)
     return render_template("index.html", cipher_text = encryption_decryption(cipher_text))
-
 
 # main
 if __name__ == "__main__":
